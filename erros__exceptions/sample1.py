923f80a5fd0fe526146e2b28b5b17756e11eddcc
Nilanjan Deb <nilanjan172nsvian@gmail.com>
2020-09-26 20:42:32 +0530

    dude error

---
+++ erros__exceptions/sample1.py
@@ -0,0 +1,17 @@
+"""
+A Very Simple Python Script
+"""
+
+
+def my_func():
+    """
+    A simple function
+    :return:
+    """
+    first = 1
+    second = 3
+    print(first)
+    print(second)
+
+
+my_func()
